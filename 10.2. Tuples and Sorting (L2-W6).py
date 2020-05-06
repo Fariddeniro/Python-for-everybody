d1d950c51e19fa16efaf0973c565be745a99a69a
Farid Farmani <faridfarmani2000@gmail.com>
2020-05-06 12:50:14 -0400

    Solution for Ex. 10.2 - Week 6 - Python Data Structures - Py4E

---
+++ 10.2. Tuples and Sorting (L2-W6).py
@@ -0,0 +1,15 @@
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+count=0
+db=dict()
+for line in handle:
+    if line.startswith('From '):
+        ly=line.rstrip()
+        words=ly.split()
+        HR=words[5].split(':')
+        hour=HR[0]
+        db[hour]=db.get(hour,0)+1
+sortdb=sorted(db.items())
+for k, v in sortdb:
+    print(k, v)
