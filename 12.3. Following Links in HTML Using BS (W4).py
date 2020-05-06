803003eb18385cffc7f9e1ad4bb33f06d5158578
Farid Farmani <faridfarmani2000@gmail.com>
2020-05-06 17:57:34 -0400

    Using Python to Access Web Data

---
+++ 12.3. Following Links in HTML Using BS (W4).py
@@ -0,0 +1,29 @@
+import urllib.request
+import urllib.parse
+import urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = 'http://py4e-data.dr-chuck.net/known_by_Olubanke.htm'
+count = int(input("Enter count: "))
+pos = int(input("Enter position:"))
+
+for i in range(count):
+    html = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('a')
+    s = []
+    t = []
+    for tag in tags:
+        x = tag.get('href', None)
+        s.append(x)
+        y = tag.text
+        t.append(y)
+
+    print(s[pos-1])
+    print(t[pos-1])
+    url = s[pos-1]
---
+++ 13.1. Extracting Data from XML (W5).py
@@ -0,0 +1,13 @@
+import urllib.request
+import urllib.parse
+import urllib.error
+import json
+
+url = 'http://py4e-data.dr-chuck.net/comments_415474.json'
+uh = urllib.request.urlopen(url)
+data = uh.read()
+info = json.loads(data)
+total = 0
+for i in range(0, len(info['comments'])):
+    total += info['comments'][i]['count']
+print(total)
